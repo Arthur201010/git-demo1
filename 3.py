bdcab0a5e16a48ec5d16f490e8aa85429ead4839
arthur <arthur@gmail.com>
2026-07-01 16:21:30 +0800

    完成連續數值輸入版本

--- 3.py
+++ 3.py
@@ -1,15 +1,19 @@
 # 1.加入例外處理
 # 2.重複輸入, 直到a=>exit 離開
 
-try:
-    a=eval(input("請輸入a: "))
-    b=eval(input("請輸入b: "))
+while True:
+    try:
+        a=input("請輸入a: ")
+        if a.lower() == "exit":
+            break
+        a=eval(a)
+        b=eval(input("請輸入b: "))
 
-    if a==b:
-        print("a等於b")
-    elif a>b:
-        print(f"a>b, 差值為{a-b}")
-    else:
-        print(f"b>a, 差值為{b-a}")
-except Exception as e:
-    print(f"輸入錯誤: {e}")
+        if a==b:
+            print("a等於b")
+        elif a>b:
+            print(f"a>b, 差值為{a-b}")
+        else:
+            print(f"b>a, 差值為{b-a}")
+    except Exception as e:
+        print(f"輸入錯誤: {e}")
