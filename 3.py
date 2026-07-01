c53b70a8cfefcb10065194baeeec1fa223f05960
arthur <arthur@gmail.com>
2026-07-01 16:01:31 +0800

    加入例外處理

--- 3.py
+++ 3.py
@@ -1,9 +1,15 @@
-a=eval(input("請輸入a: "))
-b=eval(input("請輸入b: "))
+# 1.加入例外處理
+# 2.重複輸入, 直到a=>exit 離開
 
-if a==b:
-    print("a等於b")
-elif a>b:
-    print(f"a>b, 差值為{a-b}")
-else:
-    print(f"b>a, 差值為{b-a}")
+try:
+    a=eval(input("請輸入a: "))
+    b=eval(input("請輸入b: "))
+
+    if a==b:
+        print("a等於b")
+    elif a>b:
+        print(f"a>b, 差值為{a-b}")
+    else:
+        print(f"b>a, 差值為{b-a}")
+except Exception as e:
+    print(f"輸入錯誤: {e}")
